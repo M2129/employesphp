dfc5aebf5ecf3883ffdf40b8ac45aa98a6d85404
PMSD <pdieye09@gmail.com>
2026-07-01 02:52:35 +0000

    Affiche le nom des catégories qui n'ont aucun produit.

--- procedurale.php
+++ procedurale.php
@@ -25,3 +25,17 @@ $categories = [
         "produits" => []
     ]
 ];
+
+/**
+ * Affiche le nom des catégories qui n'ont aucun produit.
+ */
+function afficheCategorieSansProduit(array $categories): void
+{
+    foreach ($categories as $categorie) {
+        if (empty($categorie["produits"])) {
+            echo $categorie["nom"] . "\n";
+        }
+    }
+}
+
+afficheCategorieSansProduit($categories);
